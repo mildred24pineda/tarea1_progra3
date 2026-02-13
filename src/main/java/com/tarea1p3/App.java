291883afaa32bebf2c616534658c1510f1b2f138
Mildred Pineda <mpinedap3@miumg.edu.gt>
2026-02-13 13:05:03 -0600

    Primer commit: subiendo proyecto arrays-java

---
+++ src/main/java/com/tarea1p3/App.java
@@ -0,0 +1,15 @@
+package com.tarea1p3;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo desde Java");
+
+		int a = 6;
+		int b = 7;
+		int resultado = a * b;
+		System.out.println("Resultado: " + resultado);
+
+	}
+
+}
